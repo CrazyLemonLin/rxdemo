a0d4352f0844baee4a14c318d89a0b8f1238869f
CYL <lemonlinchju@hotmail.com>
2017-06-26 21:41:43 +0800

    complete customer service

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getCustomers() {
+    const source = this.http.get('./assets/api/customers.json')
+      .map(res => res.json());
+
+    return source;
+  }
 }
