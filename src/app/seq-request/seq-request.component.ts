8655ae0611d4163b70b82bca9f619ecb2fe03185
CYL <lemonlinchju@hotmail.com>
2017-06-28 00:47:52 +0800

    完成 seq request component

--- src/app/seq-request/seq-request.component.ts
+++ src/app/seq-request/seq-request.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'cyl-seq-request',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seq-request.component.css']
 })
 export class SeqRequestComponent implements OnInit {
-
-  constructor() { }
+  result: any;
+  constructor(private _customerService: CustomerService, private _productService: ProductsService) { }
 
   ngOnInit() {
+    const customerSource = this._customerService.getOneCustomer();
+    const productSource = this._productService.getOneProduct();
+
+    const source =
+      customerSource
+        .switchMap(customer => productSource, (customer, product) => {
+          console.log(`${customer.name} 買了 ${product.name}`);
+            return `${customer.name} 買了 ${product.name}`;
+        });
+
+    source.subscribe(s =>{
+      console.log(s);
+      this.result = s;
+    });
   }
 
 }
